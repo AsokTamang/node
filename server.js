50ea40cce8b0a6791124b481a5528fa8bbc0d543
AsokTamang <ashoktmg2058@gmail.com>
2025-12-06 00:05:57 -0500

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-import http from 'http'
+import http from "http";
 
-const server=http.createServer((req,res)=>   
-    res.end('Hello from the server.')  //here we are sending the response of this message using the server created from the http module
-)
+const server = http.createServer(
+  (req, res) => {
+    if (req.url == "/api") {   //if the request's endpoint is api then only we are sending the response with the end
+      res.end("Hello from the server.");
+    }
+  }
+  //here we are sending the response of this message using the server created from the http module
+);
 
-server.listen(8000,()=>console.log('server is running at the port 8000'))  //and our server is running at port 8000
+server.listen(8000, () => console.log("server is running at the port 8000")); //and our server is running at port 8000
