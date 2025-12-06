983142816d7a62a59c99be4cc3c82c077c8000d0
AsokTamang <ashoktmg2058@gmail.com>
2025-12-06 00:36:58 -0500

    res methods and properties

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-import http from 'node:http'
-import { getDataFromDB } from './database/db.js'
- 
-const PORT = 8000
-const destinations =await getDataFromDB()  //as this getdatafromdb is an async function, we must use await method
+import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
-const server = http.createServer((req, res) => {
+const PORT = 8000;
 
-/*
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB(); //as this getdatafromdb is an async function, we must use await method
+  /*
 Challenge:
   1. Store our data in a const ‘destinations’.
   2. When a GET request is received to the ‘/api' endpoint, send our JSON stringified data.
     Think: What changes will you need to make to get this to work?
 */
 
-  if (req.url === '/api' && req.method === 'GET') {
-    res.end(JSON.stringify(destinations))  //and as a response we are returning the stringified version of JSON data
+  if (req.url === "/api" && req.method === "GET") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(destinations)); //and as a response we are returning the stringified version of JSON data
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type','application/json');
+    res.end(
+      JSON.stringify({
+        error: "not found",
+        message: "The requested route does not exist",
+      })
+    );
   }
-})
+});
 
-server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
