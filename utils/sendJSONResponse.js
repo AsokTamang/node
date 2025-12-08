d64fcd124b8135af9185aa627e24031525f9da87
AsokTamang <ashoktmg2058@gmail.com>
2025-12-08 13:08:25 -0500

    CORS integration in node env

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,8 @@
-export const sendJSONResponse = (res, code, payload) => { //here we are making an arrow function of utils where response , code and payload must be passed 
+export const sendJSONResponse = (res, code, payload) => {
+  //here we are making an arrow function of utils where response , code and payload must be passed
   res.statusCode = code;
+  res.setHeader("Access-Control-Allow-Origin", "*"); //this allows our server to be accessed from any frontends
+  res.setHeader("Access-Control-Allow-Methods", "GET"); //this only allows the GET method in our server endpoint
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(payload));  //here res.end is for sending the output
+  res.end(JSON.stringify(payload)); //here res.end is for sending the output
 };
